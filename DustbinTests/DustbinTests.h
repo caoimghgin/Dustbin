91d7efe20ee3704225a2662582d561e95f0254ae
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-22 18:20:07 -0400

    Initial Commit

---
+++ DustbinTests/DustbinTests.h
@@ -0,0 +1,13 @@
+//
+//  DustbinTests.h
+//  DustbinTests
+//
+//  Created by Kevin Muldoon on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DustbinTests : SenTestCase
+
+@end
