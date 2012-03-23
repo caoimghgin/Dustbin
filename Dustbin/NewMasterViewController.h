102e13d0f18db7f5346627681532c7b7c6358f04
Jinchuan <jinchuanshi86@gmail.com>
2012-03-23 16:21:44 -0400

    new master

---
+++ Dustbin/NewMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewMasterViewController.h
+//  Dustpan
+//
+//  Created by Jin Shi on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewMasterViewController : UIViewController
+
+@end
---
+++ Dustbin/NewMasterViewController.m
@@ -0,0 +1,43 @@
+//
+//  NewMasterViewController.m
+//  Dustpan
+//
+//  Created by Jin Shi on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NewMasterViewController.h"
+
+@interface NewMasterViewController ()
+
+@end
+
+@implementation NewMasterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
