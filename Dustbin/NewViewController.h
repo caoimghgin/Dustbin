0059f0605a450952e0db7d6a351a4c79d9bb5555
Jinchuan <jinchuanshi86@gmail.com>
2012-03-23 14:52:38 -0400

    New View controller

---
+++ Dustbin/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  Dustpan
+//
+//  Created by Jin Shi on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
