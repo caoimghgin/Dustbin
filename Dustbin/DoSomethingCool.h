fb362b4f97ba09353ee349f90cb176737045624d
caoimghgin <caoimghgin@gmail.com>
2012-03-23 09:18:33 -0400

    I made something cool

---
+++ Dustbin/DoSomethingCool.h
@@ -0,0 +1,13 @@
+//
+//  DoSomethingCool.h
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DoSomethingCool : NSObject
+
+@end
---
+++ Dustbin/DoSomethingCool.m
@@ -0,0 +1,13 @@
+//
+//  DoSomethingCool.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DoSomethingCool.h"
+
+@implementation DoSomethingCool
+
+@end
