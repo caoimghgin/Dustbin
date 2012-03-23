498495cff10ba46c2bc599cad7c1d917f3f50476
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-23 17:45:55 -0400

    Branch Charlie

---
+++ Dustbin/CharlieClass.h
@@ -0,0 +1,13 @@
+//
+//  CharlieClass.h
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CharlieClass : NSObject
+
+@end
---
+++ Dustbin/CharlieClass.m
@@ -0,0 +1,13 @@
+//
+//  CharlieClass.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CharlieClass.h"
+
+@implementation CharlieClass
+
+@end
