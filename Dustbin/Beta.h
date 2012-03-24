bae2e1a3decfecef32f154db9eb00b5ef1998cc8
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-24 18:35:10 -0400

    Just some new files.

---
+++ Dustbin/Beta.h
@@ -0,0 +1,13 @@
+//
+//  Beta.h
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Beta : NSObject
+
+@end
---
+++ Dustbin/Beta.m
@@ -0,0 +1,13 @@
+//
+//  Beta.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Beta.h"
+
+@implementation Beta
+
+@end
