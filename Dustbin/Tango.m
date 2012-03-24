2d1831eacaf326c946b5398ef8e3165f377cbd2c
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-24 18:53:05 -0400

    Nice stuff to add here in Tango

---
+++ Dustbin/Tango.m
@@ -0,0 +1,13 @@
+//
+//  Tango.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tango.h"
+
+@implementation Tango
+
+@end
