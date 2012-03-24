fc73a741127dc3c8671cef50495acf3c771d3408
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-24 18:18:29 -0400

    Added new object called Foxtrot

---
+++ Dustbin/Foxtrot.m
@@ -0,0 +1,13 @@
+//
+//  Foxtrot.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Foxtrot.h"
+
+@implementation Foxtrot
+
+@end
