f8cd123d231a964dda8ee1cb12ab7cf7bdc742a8
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-24 18:59:06 -0400

    Revert "Revert "Nice stuff to add here in Tango""
    This reverts commit f30ab9959eed27b15bedb20a1a2bff06c1aacfc8.

---
+++ Dustbin/Tango.h
@@ -0,0 +1,13 @@
+//
+//  Tango.h
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tango : NSObject
+
+@end
