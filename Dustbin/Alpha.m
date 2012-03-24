6d9408541ceb6d95b4354e5eef9dfd0a47f3d39e
Kevin Muldoon <caoimghgin@gmail.com>
2012-03-24 18:28:00 -0400

    Added Alpha objects

---
+++ Dustbin/Alpha.m
@@ -0,0 +1,13 @@
+//
+//  Alpha.m
+//  Dustpan
+//
+//  Created by Kevin Muldoon on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Alpha.h"
+
+@implementation Alpha
+
+@end
